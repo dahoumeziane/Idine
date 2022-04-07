2057955e0efec3939a3710bcf869a9b1408e6e10
Dahou meziane <hd_meziane@esi.dz>
2022-04-07 12:55:06 +0100

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Idine
+//
+//  Created by Dahou Meziane on 7/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    var body: some View {
+        NavigationView{
+            List {
+                ForEach(menu){ section in
+                    Section(header: Text(section.name) ){
+                        ForEach(section.items){ item in
+                            Text(item.name)
+                            
+                        }
+                    }
+                   
+                   
+                }
+               
+                
+            }
+            .navigationTitle("Menu")
+        }
+       
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IdineApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdineApp.swift
+//  Idine
+//
+//  Created by Dahou Meziane on 7/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct IdineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
